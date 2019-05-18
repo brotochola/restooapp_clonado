7d278851381ace5d24cecc9c4c61f29f71f74e1b
facundo <saiegh@gmail.com>
2019-05-18 18:32:29 -0300

    el resto del front

---
+++ front/cocinero/cocinero.js
@@ -0,0 +1,146 @@
+console.log("cocinero.js")
+
+const tiempoDelay = 10000;
+
+$(document).ready(function() {
+    cargarEstado()
+
+});
+
+var estadoActual = {};
+
+function cargarEstado() {
+    console.log("cargar estado")
+
+    $.ajax({
+        url: "../../server/cocinero/estado",
+        headers: {
+            token: localStorage[usuarioLogueado_ls]
+        },
+        success: function(e) {
+            try { estadoActual = JSON.parse(e) } catch (e) { estadoActual = e }
+            actualizarEstadoActual(estadoActual)
+        }
+    })
+
+
+
+
+    setTimeout(cargarEstado, tiempoDelay)
+}
+
+
+function actualizarEstadoActual(ea) {
+    if (ea.notif.length > 0) {
+        mostrarNotif(ea.notif[0])
+    }
+
+    $("#contenedorPedidosActuales").html("")
+    for (var i = 0; i < ea.pedidos.length; i++) {
+        $("#contenedorPedidosActuales").append(armarPedidoActual(ea.pedidos[i]))
+    }
+}
+
+function mostrarNotif(notif) {
+    console.log("lanzando notificacion")
+    rol = JSON.parse(localStorage[usuarioLogueado_ls]).rol
+    $("#rolEmple").html(lugaresDeTrabajoRoles[rol])
+    $("#mesaNum").html(notif.mesa)
+    $("#pedidoNum").html(notif.pedido)
+    $("#horaPedido").html("Hora Pedido: " + notif.hora_pedido)
+
+    keys = Object.keys(notif.prods)
+    $("#contPedidosNotif").html("")
+    for (var i = 0; i < keys.length; i++) {
+
+        $("#contPedidosNotif").append(armarProdNotif(keys[i], notif.prods[keys[i]]))
+    }
+
+
+    $("#notifCocinero").fadeIn()
+}
+
+function armarProdNotif(id, prod) {
+    html = ""
+    html += ' <div class="col-6">';
+    html += '<div class="row">';
+    html += '<div class="col-4">';
+    html += '<span class="numero-de-pedido-cocinero">#<span id="idProducto">' + id + '</span></span>';
+    html += '</div> ';
+    html += '<div class="col-8 nombreProd">' + prod + '</div>';
+    html += '</div>';
+    html += '</div>';
+    return html
+}
+
+function aceptarPedido(id) {
+    $("#notifCocinero").fadeOut()
+    $.ajax({
+        url: "../../server/cocinero/aceptarPedido",
+        headers: {
+            token: localStorage[usuarioLogueado_ls]
+        },
+        data: {
+            "idPedido": id
+        },
+        success: function(e) {
+            try { estadoActual = JSON.parse(e) } catch (e) { estadoActual = e }        	
+        	actualizarEstadoActual(estadoActual)
+        }
+    })
+}
+
+function armarProdPedidoActual(id, nombre) {
+    var html = ""
+    html += '<div class="col-12 prod">'
+    html += '<div class="row">'
+    html += '<div class="col-2">'
+    html += '<span class="numero-de-pedido-cocinero">#' + id + '</span>'
+    html += '</div>'
+    html += '<div class="col-10 text-left">' + nombre + ' </div>'
+    html += '</div>'
+    html += '<hr>'
+    html += '</div>'
+    return html;
+}
+
+
+function pedidoListo(id) {
+    console.log("el pedido " + id + " esta listo")
+    $(".unPedido."+id).remove() //esto es de mentirita
+
+}
+
+function armarPedidoActual(pedido) {
+
+
+    console.log(pedido)
+
+    var html = '  <!-- un pedido -->'
+    html += '            <div class="unPedido '+pedido.id+'">'
+    html += '                  <div class="row listaPedidosPendientes">'
+    html += '  <div class="col-5">'
+    html += '  <img src="../media/Img/pedido-black.png" class="pedido-pendiente">'
+    html += '  <span class="n-pedido-p">PEDIDO #<strong>' + pedido.id + '</strong></span>'
+    html += '  </div>'
+    html += '  <div class="col-5">'
+    html += '  <img src="../media/Img/mesa.png" class="mesa-pendiente">'
+    html += '  <span class="n-mesa-p">MESA #<strong>' + pedido.mesa + '</strong></span>'
+    html += '  </div>'
+    html += '  <div class="col-2 check-verde-pedidos">'
+    html += '  <img src="../media/Img/check-verde.png" onclick="pedidoListo(' + pedido.id + ')">'
+    html += '  </div>'
+    html += '  </div>'
+    html += '  <div class="row list-pedidos-pendientes">'
+
+    keys = Object.keys(pedido.prods)
+    for (var i = 0; i < keys.length; i++) {
+        html += armarProdPedidoActual(keys[i], pedido.prods[keys[i]])
+    }
+
+    html += '  </div>'
+    html += '  </div>'
+    html += '  <!-- fin pedido-->'
+    console.log()
+    return html
+}
---
+++ front/mozo/estado.js
@@ -0,0 +1,176 @@
+console.log("estado.js")
+
+var delayTraerEstado = 5000;
+var tiempoNotificacion = 3500;
+var estadoActual = {};
+var mesas;
+var mesasCargadas = 0;
+var docReady = 0;
+var notificacionesPendientes = [];
+
+
+//ni bien empieza cargo las mesas,
+//dps me manejo solo con el array q trae el servicio de la API: /mozo/estado
+//cada 5 segundos
+
+$.ajax({
+    url: "../../server/mozo/mesas/lista",
+    type: "get", //esto es solo porq estoy no usando un backend de verdad
+    headers: {
+        token: localStorage[usuarioLogueado_ls]
+    },
+    success: function(e) {
+        //  console.log(e)
+        try { mesas = JSON.parse(e) } catch (e) { mesas = e }
+        localStorage["mesas_" + nombreDelSitio] = e;
+        mesasCargadas = 1;
+        if (docReady == 1) setup()
+    }
+})
+
+function setup() {
+    cargarEstadoMozo();
+    armarGraficoMesas();
+}
+
+function logOut() {
+    mostrarModalConfirmacion("Seguro que querés desloguearte?", "si", "no", function() {
+        localStorage[usuarioLogueado_ls] = null;
+        window.location.href = "index.html"
+    })
+}
+
+$(document).ready(function() {
+    docReady = 1;
+    if (mesasCargadas == 1) setup()
+    console.log("doc ready estado mozx");
+
+
+
+    //populo cosas:
+    try {
+        console.log(localStorage[usuarioLogueado_ls])
+        obj = JSON.parse(localStorage[usuarioLogueado_ls])
+        $("#nombreMozo").html(obj.nombre)
+
+    } catch (e) {
+        console.log(e)
+        $("#nombreMozo").html(localStorage[usuarioLogueado_ls].nombre)
+    }
+
+
+    //listeners
+
+
+    //agrego cachos:
+    agregarCachoABody("modalConfirmacion.html")
+    agregarCachoABody("verEstadoMesaMozo.html")
+    agregarCachoABody("verAgregarPedidoMozo.html")
+    agregarCachoABody("nuevoPedidoAgregarProductos.html")
+    agregarCachoABody("nuevoCliente.html")
+
+
+
+
+
+});
+
+function verMesa(id) {
+    //aca la funcion ver mesa es esta
+    //en la pantalla de mesas la funcion es otra
+    console.log(estadoActual)
+    console.log(estadoActual.estados)
+    if (estadoActual == undefined || estadoActual.estados == undefined) {
+        mostrarModalConfirmacion("Esperando data... momento", "ok")
+        return;
+    }
+
+    if (estadoActual.estados[id] == 0 || estadoActual.estados[id] == undefined) {
+        //la mesa esta libre
+        mostrarModalNuevoCliente(id); //y le mando el id de la mesa
+    } else {
+        mostrarEstadoMesaMozo(id)
+    }
+
+
+
+}
+
+function cargarEstadoMozo() {
+    console.log("cargar estado mozo!")
+    //esta funcion se ejecuta cada 5 segundos
+    if (mesas == undefined || mesas.length == 0 || isNaN(mesas[0].id) || mesas[0].id == undefined) return; //si no estan cargadas las mesas, el grafico, esta funcion no anda
+
+    $.ajax({
+        url: "../../server/mozo/estado",
+        success: function(e) {
+            try { estadoActual = JSON.parse(e) } catch (e) { estadoActual = e }
+            console.log(estadoActual)
+            //console.log(estadoActual)
+            $("#recaudacionDia").html("$" + estadoActual.facturacionDia);
+            $("#cantComensalesDia").html(estadoActual.cubiertosDelDia);
+            //estados de las mesas
+            // console.log(mesas)
+            // console.log(estadoActual)
+            var idsMesas = Object.keys(estadoActual.estados);
+
+            for (var i = 0; i < idsMesas.length; i++) {
+                $(".mesa." + idsMesas[i] + " img.mesaImg")[0].src = "../mesas/" + mesas[mesaId2i(parseInt(idsMesas[i]))].sillas + "/" + estadoMesas[estadoActual.estados[idsMesas[i]]] + ".png";
+            }
+
+            //veo si hay notificaciones:
+            if (estadoActual.notif.length > 0) {
+                for (var i = 0; i < estadoActual.notif.length; i++) {
+                    n = estadoActual.notif[i];
+                    cargarNotificacion(n)
+                }
+            }
+            mostrarSiguienteNotificacion()
+            //espera y pide el estado al server de nuevo
+            setTimeout(cargarEstadoMozo, delayTraerEstado)
+        }
+    }) //ajax
+
+
+}
+
+
+function mostrarSiguienteNotificacion() {
+var muestro=1;
+    $(".fondo-modal").each(function(k,v){
+          if($(v).is(":visible") ==true) muestro=0
+    })
+  
+    //si estan todos invisible, sigue de largo y muestra la siguiente notif
+    if (muestro==1 && notificacionesPendientes.length > 0) mostrarNotificacion(notificacionesPendientes.pop())
+
+}
+
+
+function mostrarNotificacion(n) {
+    try {
+        $("#donde").html(n.donde.toUpperCase())
+        $("#idPedido").html("#" + n.pedido)
+        $("#idMesa").html("#" + n.mesa)
+
+        $("#notificacionesMozo").fadeIn()
+        setTimeout(ocultarNotificaciones, tiempoNotificacion)
+    } catch (e) { console.log("la notificacion no está completa y nos e muestra") }
+}
+
+function ocultarNotificaciones() {
+    $("#notificacionesMozo").fadeOut()
+}
+
+function cargarNotificacion(n) {
+
+    for (var i = 0; i < notificacionesPendientes.length; i++) {
+        // console.log(notificacionesPendientes[i].mesa, n.mesa, notificacionesPendientes[i].pedido, n.pedido, notificacionesPendientes[i].donde, n.donde)
+        if (notificacionesPendientes[i].mesa == n.mesa && notificacionesPendientes[i].pedido == n.pedido && notificacionesPendientes[i].donde == n.donde) {
+            //  console.log("son iguales")
+            return;;
+        }
+    }
+    //no esta
+    notificacionesPendientes.push(n)
+}
