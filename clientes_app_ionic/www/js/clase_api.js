bf3920143ba61ac9f1ee1269ba01cbd9e3f51b3f
gurpreet808 <gurpreets808@gmail.com>
2019-06-17 23:37:14 -0300

    App cliente funcionando login, falta retocar UI

---
+++ clientes_app_ionic/www/js/clase_api.js
@@ -0,0 +1,135 @@
+console.log("clase_api.js")
+
+class API {
+    constructor() {
+
+        //local
+        // this.urlServer="../../Resto/API/";
+        //web
+
+        //http://darodarioli.tech/restoapp2/Resto/API/
+        // this.urlServer= "http://darodarioli.tech/restoapp2/Resto/API/";
+
+
+        if (window.hasOwnProperty("cordova")) this.urlServer = "http://pixeloide.com/restoApp/API/";
+        else this.urlServer = "../../Resto/API/";
+
+        // this.urlServer="http://pixeloide.com/restoApp/API/"
+
+        //ESTOS DATOS VIENEN DEL SERVER Y QUEDAN TODOS ACA:
+        this.empleados = null;
+        this.mesas = null;
+        this.productos = null;
+        this.estadoAnteriorMozo = null;
+        this.estadoMozo = null;
+        this.estadoCocinero = null;
+
+    }
+
+    mozoMandaPedido(ped, cb) {
+        let pedidoParaMandar = {}
+        pedidoParaMandar.id_mesa = mesaActiva.id_mesa;
+        pedidoParaMandar.id_cliente = mesaActiva.cliente.id_cliente
+        pedidoParaMandar.id_cliente_visita = mesaActiva.clienteVisita.id_cliente_visita;
+        pedidoParaMandar.productos = []
+        pedidoParaMandar.cantidades = []
+        //los paso al formato q requiere el backend:
+        for (let i = 0; i < ped.productos.length; i++) {
+            pedidoParaMandar.productos.push(ped.productos[i].id_producto)
+            pedidoParaMandar.cantidades.push(ped.productos[i].cantidad)
+        }
+
+        $.ajax({
+            url: this.urlServer + "mozo/agregarPedido",
+            type: "post",
+            dataType: "json",
+            headers: {
+                token: localStorage[usuarioLogueado_ls]
+            },
+            data: pedidoParaMandar,
+            success: function (e) {
+
+                console.log(e)
+
+                if (cb instanceof Function) cb(e);
+
+            }, error: e => console.log(e)
+        })
+    }
+
+    traerProductos(cb) {
+
+        $.ajax({
+            //url: "../../server/admin/productos/lista",
+            url: this.urlServer + "producto/listado",
+            type: "post", //esto es solo porq estoy no usando un backend de verdad
+            dataType: "json",
+            headers: {
+                token: localStorage[usuarioLogueado_ls]
+            },
+            success: (e) => {
+                console.log(e)
+                this.productos = e
+
+                localStorage["productos_" + nombreDelSitio] = e;
+
+                if (cb instanceof Function) cb();
+            }, error: e => console.log(e)
+        })
+    }
+
+    traerUnaMesa(id, cb) {
+
+        $.ajax({
+            url: this.urlServer + "mesas/id/" + id,
+            headers: {
+                token: localStorage[usuarioLogueado_ls]
+            },
+            dataType: "json",
+            success: function (e) {
+                // console.log(e)
+                let mesa = e
+                // console.log(mesa)
+                if (cb instanceof Function) cb(mesa);
+            }, error: (e) => {
+                console.log(e)
+            }
+        }); //ajax
+    }
+
+    cargarPedidosMozo(cb) {
+        $.ajax({
+            url: this.urlServer + "mozo/traerMisPedidos",
+            type: "post",
+            dataType: "json",
+            headers: {
+                token: localStorage[usuarioLogueado_ls]
+            },
+            success: (e) => {
+                console.log(e)
+                this.pedidosAnteriorMozo = this.pedidosMozo;
+                this.pedidosMozo = e;
+                if (cb instanceof Function) cb(e);
+            },
+            error: (e) => {
+                console.log(e.responseText)
+            }
+        })
+    }//estdo mozo
+
+    logueo(mail, dni, cb) {
+        console.log("Clase api: " + mail, dni, "server: " + this.urlServer)
+        $.ajax({
+            url: this.urlServer + "cliente/login2",
+            type: "post",
+            dataType: "json",
+            data: {
+                "email": mail,
+                "dni": dni
+            },
+            success: (e) => {
+                if (cb instanceof Function) cb(e);
+            }, error: e => console.log(e)
+        })
+    }
+}
---
+++ clientes_app_ionic/www/js/clase_app.js
@@ -0,0 +1,177 @@
+console.log("clase_app.js")
+class App {
+    //todo lo q tenga q ver con navegacion UI lo pongo aca
+    constructor(nombre) {
+        this.$contenido = $("#contenido")
+        //this.partesActivas= []
+        this.nombre = nombre
+        this.partes = [];
+        this.$negroIndex = $("#negroIndex");
+        this.$loading = $("#loading");
+        this.$videoSplash = $("video#videoSplash");
+    }
+
+    quePasaDespuesDeLogin() {
+        console.log("Acá UI cliente");
+
+        //Revisar que tiene mesa asignada
+        if (true) {
+            this.traerHTML("partes/verEstadoMesaCliente.html", false, true, "mesaCliente", false);
+        } else {
+            //Algún botón para QR solicitar mesa
+        }
+
+        //Ver qué opciones del footer
+        //setTimeout(() => { this.mostrarFooterAdmin() }, 200)//lo atamo con alambre
+    }
+
+    init() {
+        console.log("APP INIT")
+        //MODALES:
+        /* this.traerHTML("partes/verEstadoMesa.html", true, true, "verEstadoMesa", true)
+        this.traerHTML("partes/agregarVerEmpleado.html", true, true, "verEmpleado", true)
+        this.traerHTML("partes/verAgregarPedido.html", true, true, "modalVerPedido", true)
+
+        this.traerHTML("partes/agregarVerProducto.html", true, true, "modalVerAgregarProducto", true)
+        this.traerHTML("partes/agregarVerMesa.html", true, true, "agregarVerMesa", true)
+        this.traerHTML("partes/modalConfirmacion.html", true, true, "modalConfirm", false)
+
+        this.traerHTML("partes/nuevoPedidoAgregarProductos.html", true, true, "nuevoPedidoAgregarProductos", true)
+        this.traerHTML("partes/nuevoCliente.html", true, true, "modalNuevoCliente", true) */
+        this.traerHTML("partes/perfilesDePrueba.html", true, true, "modalPerfilesDePrueba", true)
+
+        //FOOTER
+        /* this.traerHTML("partes/footerAdmin.html", true, true, "footerAdmin", true, "fija", () => {
+            setTimeout(() => { this.$footerAdmin = $("#footerAdmin"); }, 500);
+        }) */
+
+        /* this.traerHTML("partes/footerMozo.html", true, true, "footerMozo", true, "fija", () => {
+            setTimeout(() => { this.$footerMozo = $("#footerMozo"); }, 500);
+        }) */
+
+        //LOGIN LO TRAE NO COMO APPEND Y NO OCULTO
+        this.traerHTML("partes/login.html", false, true, "login", false)
+
+        if (cliente.tokenValido()) {
+            setTimeout(() => { this.quePasaDespuesDeLogin() }, 600);
+        }
+    }
+
+    terminoVideoSplash() {
+        this.mostrarLoading()
+        this.$contenido.show();
+        this.$videoSplash.hide();
+        setTimeout(() => { this.ocultarLoading() }, 500);
+    }
+
+    getGPS(cb) {
+        // if(!this.esApp()){
+        navigator.geolocation.getCurrentPosition((e) => {
+            console.log("LAT=" + e.coords.latitude);
+            console.log("LNG=" + e.coords.longitude);
+            //estan son globales:
+            this.latitud = e.coords.latitude;
+            this.longitud = e.coords.longitude;
+            if (cb != null && cb != undefined) cb();
+        }, function (e) {
+            bootbox.alert("problema accediendo al GPS. " + JSON.stringify(e));
+        });
+    }
+
+    estaLaParte(url) {
+        for (var i = 0; i < this.partes.length; i++) {
+            if (this.partes[i].url == url) return true
+        }
+        return false;
+    }
+
+    mostrarLoading() {
+        this.$loading.show();
+    }
+    ocultarLoading() {
+        this.$loading.fadeOut();
+    }
+
+    esconderTodasLasPartes() {
+        for (let i = 0; i < this.partes.length; i++) {
+            if (this.partes[i].$el.hasClass("fija")) continue;
+            this.partes[i].$el.hide()
+        }
+    }
+
+    esconderUnaParte(url, id) {
+        for (let i = 0; i < this.partes.length; i++) {
+            if (this.partes[i].url == url || this.partes[i].id == id) {
+                this.partes[i].$el.hide();
+            }
+
+        }
+    }
+
+    mostrarFooterAdmin() {
+        this.$footerAdmin.show();
+    }
+    ocultarFooterAdmin() {
+        this.$footerAdmin.hide();
+    }
+
+    mostrarFooterMozo() {
+        this.$footerMozo.show();
+    }
+    ocultarFooterMozo() {
+        this.$footerMozo.hide();
+    }
+
+    traerHTML(url, append, forzarNoCache, id, traerloOculto, clase, cb) {
+        //  console.log("#### TRAER HTML ", url, append, forzarNoCache, id)
+        if (this.estaLaParte(url)) {
+            //  console.log("## esta la parte")
+            for (let i = 0; i < this.partes.length; i++) {
+                if (this.partes[i].url == url) {
+                    if (!append) {
+                        this.esconderTodasLasPartes();
+                    }
+                    //console.log(this.partes[i].$el)
+                    this.partes[i].$el.show();
+                    app.ocultarLoading()
+                    return;
+                }
+            }
+        }
+        //  console.log("### NO ESTA LA PARTE")
+        let urlFinal;
+        if (forzarNoCache != true && forzarNoCache != 1) {
+            urlFinal = url;
+        } else {
+            urlFinal = url + "?bla=" + Math.random()
+        }
+        //si tiene q traer la parte
+        if (!append) app.mostrarLoading()
+        $.ajax({
+            url: urlFinal,
+            success: (e) => {
+                let span = document.createElement("parte"); //me gusta q se llamen asi
+                span.id = id
+                span.innerHTML = e;
+                if (append != 1 && append != true) {
+                    this.esconderTodasLasPartes();
+                }
+                this.partes.push({ url: url, id: id, $el: $(span) })
+                if (traerloOculto == true) span.style.display = "none"
+
+                if (cb instanceof Function) cb();
+                if (checkStr(clase)) $(span).addClass("fija")
+                this.$contenido.append(span)
+                app.ocultarLoading()
+            }, //fin success
+            error: () => {
+                app.ocultarLoading()
+            }
+        }) //ajax
+    }
+
+    esApp() {
+        return window.hasOwnProperty("cordova");
+    }
+
+}
