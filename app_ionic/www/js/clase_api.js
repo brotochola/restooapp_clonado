c2740a9972e3916cb13e2789d8b695bc7e659051
DaroDarioli <dario.olinuck@gmail.com>
2019-06-28 20:32:57 -0300

    subo qr propina

--- app_ionic/www/js/clase_api.js
+++ app_ionic/www/js/clase_api.js
@@ -7,11 +7,11 @@ class API {
      
 
 
-         if(window.hasOwnProperty("cordova"))  this.urlServer="http://pixeloide.com/restoApp/API/";
-           else this.urlServer ="../../Resto/API/";
+        //  if(window.hasOwnProperty("cordova"))  this.urlServer="http://pixeloide.com/restoApp/API/";
+        //    else this.urlServer ="../../Resto/API/";
      
      
-      //    this.urlServer = "http://pixeloide.com/restoApp/API/"
+          this.urlServer = "http://pixeloide.com/restoApp/API/"
 
 
       
--- clientes_app_ionic/www/js/clase_api.js
+++ clientes_app_ionic/www/js/clase_api.js
@@ -2,8 +2,8 @@ console.log("clase_api.js")
 
 class API {
     constructor() {
-        if (window.hasOwnProperty("cordova")) this.urlServer = "http://pixeloide.com/restoApp/API/";
-        else this.urlServer = "../../Resto/API/";
+        // if (window.hasOwnProperty("cordova")) this.urlServer = "http://pixeloide.com/restoApp/API/";
+        // else this.urlServer = "../../Resto/API/";
 
         this.urlServer="http://pixeloide.com/restoApp/API/"
 
